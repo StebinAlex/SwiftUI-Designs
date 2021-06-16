658df51ffda277f8dd58bfa39cdf53b132661e55
Stebin Alex <stebinalex96@gmail.com>
2021-06-16 07:15:49 +0530

    Add files via upload

---
+++ Facebook-SwiftUI/FacebookClone/FacebookClone/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  FacebookClone
+//
+//  Created by Stebin Alex on 14/06/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
